ffada7e27aa823c19efb2a020a85704efa847702
unknown <oskar.kopac@gmail.com>
2025-04-16 17:48:15 +0200

    nvjđoaivnas

--- StreetBros/StreetBros.py
+++ StreetBros/StreetBros.py
@@ -2,6 +2,10 @@ import pygame #type: ignore
 import sys #type:ignore
 import time
 import random
+from krogla import Krogla
+from strela import Strela
+from borec import Borec
+
 pygame.init()
 pygame.display.set_caption("StreetBros - Oskar Kopač, R2C")
 
@@ -40,205 +44,11 @@ game_over_bg = pygame.transform.scale(pygame.image.load("StreetBros/efekti/game_
 pritisni_enter = pygame.transform.scale(pygame.image.load("StreetBros/efekti/pritisni_enter.png"), (300,200))
 game_over_icon = pygame.transform.scale(pygame.image.load("StreetBros/efekti/game_over_icon.png"),(500,500))
 
-class Krogla:
-    def __init__(self, x, y, smer):
-        self.x = x
-        self.y = y
-        self.smer = smer  # 1 za desno, 0 za levo
-        self.hitrost = 15
-        self.slika = pygame.transform.scale(pygame.image.load("StreetBros/player1/special/ogenj_1.png"), (150, 150))
-        
-    def premik_desno(self):
-        self.x += 10
-    
-    def premik_levo(self):
-        self.x-= 10
 
-    def posodobi(self):
-        if self.smer == 1:
-            self.premik_desno()
-        else:
-            self.premik_levo()
-        
-    def je_izven_zaslona(self, sirina_zaslona):
-        if self.x < -100 or self.x > sirina_zaslona + 100:
-            return True
-        return False
-        
-    def trk_z_igralcem(self, igralec):
-        if igralec.x - 50 < self.x < igralec.x + 350 and igralec.y - 50 < self.y < igralec.y + 350:
-            return True
-        else:
-            return False
 
-class Strela:
-    def __init__(self, x, y, smer):
-        self.x = x
-        self.y = y
-        self.smer = smer  # 1 za desno, 0 za levo
-        self.hitrost = 20
-        self.slika = pygame.transform.scale(pygame.image.load("StreetBros/player2/strela/strela_1.png"), (150, 150))
-        
-    def premik_desno(self):
-        self.x += 15
-    
-    def premik_levo(self):
-        self.x -= 15
 
-    def posodobi(self):
-        if self.smer == 1:
-            self.premik_desno()
-        else:
-            self.premik_levo()
-        
-    def je_izven_zaslona(self, sirina_zaslona):
-        if self.x < -200 or self.x > sirina_zaslona + 200:
-            return True
-        return False
-        
-    def trk_z_igralcem(self, igralec):
-        if igralec.x - 50 < self.x < igralec.x + 350 and igralec.y - 50 < self.y < igralec.y + 350 and igralec.crouch != True:
-            return True
-        else:
-            return False
 
-class Borec:
-    def __init__(self, ime, health, avatar, Tocka_na_udarec, hitrost, pozicija, blok, udarec, tek, x, y, smer, walk, skok, hitrost_skoka, special):
-        self.tek = tek
-        self.ime = ime
-        self.health = health
-        self.max_health = health  
-        self.avatar = avatar
-        self.Tocka_na_udarec = Tocka_na_udarec
-        self.hitrost = hitrost
-        self.pozicija = pozicija
-        self.blok = blok
-        self.udarec = udarec
-        self.smer = smer
-        self.walk = walk
-        self.skok = skok
-        self.hitrost_skoka = 0
-        self.special = special
-        self.x = x
-        self.y = y
-        self.krogle = []  
-        self.cooldown = 0  
-        self.krogle = []  
-        self.cooldown_krogle = 0
-        self.strele = []
-        self.cooldown_strela = 0
-        self.hitbox = (self.x - 200, self.y-200)
-        self.crouch = False
-        self.udarec_animacija = False
-        self.udarec_frame = 0
-        self.udarec_cooldown = 0
-        self.zadnji_udarec_frame = -1 
-        self.score = 0
-    
-    def zbij_health(self, vrednost):
-        self.health -= vrednost
 
-    def ustvari_strelo(self):
-        if self.cooldown_strela <= 0:
-            zacetni_x = self.x + 150 if self.smer == 1 else self.x - 50
-            nova_strela = Strela(zacetni_x, self.y + 100, self.smer)
-            self.strele.append(nova_strela)
-            self.cooldown_strela = 100 
-            return True
-        return False
-
-    def ustvari_kroglo(self):
-        if self.cooldown_krogle <= 0:
-            zacetni_x = self.x + 150 if self.smer == 1 else self.x - 50
-            nova_krogla = Krogla(zacetni_x, self.y + 100, self.smer)
-            self.krogle.append(nova_krogla)
-            self.cooldown_krogle = 30  
-            return True
-        return False
-
-    def posodobi_krogle(self, sirina_zaslona):
-        if self.cooldown_krogle > 0:
-            self.cooldown_krogle -= 1
-        
-        for krogla in self.krogle[:]:
-            krogla.posodobi()
-            
-            if krogla.je_izven_zaslona(sirina_zaslona):
-                self.krogle.remove(krogla)
-                continue
-                
-            if krogla.trk_z_igralcem(Player2):
-                Player1.special= False
-                if Player2.blok == False or (Player2.blok == True and Player2.smer == Player1.smer):
-                    Player2.zbij_health(10)
-                else:
-                    pass
-                    
-                self.krogle.remove(krogla)
-
-    def posodobi_strele(self, sirina_zaslona):
-        if self.cooldown_strela > 0:
-            self.cooldown_strela -= 1
-        
-        for strela in self.strele[:]:
-            strela.posodobi()
-            
-            if strela.je_izven_zaslona(sirina_zaslona):
-                self.strele.remove(strela)
-                continue
-                
-            if strela.trk_z_igralcem(Player1):
-                Player2.special = False
-                if Player1.blok == False or (Player1.blok == True and Player1.smer == Player2.smer):
-                    Player1.zbij_health(10)
-                else:
-                    pass
-                self.strele.remove(strela)
-    
-
-    def get_rezilo_pozicija(self):
-        if self.smer == 1:  # Desno
-            return (self.x + 200, self.y + 100, self.x + 300, self.y + 250)
-        else:  # Levo
-            return (self.x, self.y + 100, self.x + 100, self.y + 250)
-
-    def premik_levo(self):
-        self.x -= 20
-        
-    def premik_desno(self):
-        self.x += 20
-    
-    def poklekni(self):
-        self.y += 10
-        
-    def blok(self):
-        self.blok = True
-        
-    def konec_bloka(self):
-        self.blok = False
-        
-    def hoja(self):
-        self.walk = True
-        
-    def skoci(self):
-        if not self.skok:
-            self.skok = True
-            self.hitrost_skoka = -15
-
-    def udari(self, other):
-        if (other.x > self.x and 30 < (other.x - self.x) < 60) or (other.x < self.x and  30 < (self.x - other.x) < 60):
-            if other.blok == False or(self. smer == other.smer and other.blok == True):
-                other.zbij_health()
-
-    def preveri_border(self):
-        if self.x <= -50:
-            self.x = -50
-        elif self.x >= length-300:
-            self.x = length-300
-
-    def preveri_health(self):
-        if self.health <= 0:
-                screen.blit()
 
 class MAP:
     def __init__(self, picture, max_x, min_x, max_y, min_y, sound):
